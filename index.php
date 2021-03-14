3b2b603738b4dc9d68933f382c4b19ab58924d57
Florian <gerkensmeier.florian@gmx.de>
2021-03-14 15:24:45 +0100

    Notes

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
-echo "hi";
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "localhost/147011778637856768/73e7d032b1fdf457efef44916c79a424b2d99d25"); 
+curl_setopt($ch, CURLOPT_PORT, 9999);
+$output = curl_exec($ch);
+curl_close($ch);   
 
+return $output;
 ?>
+
